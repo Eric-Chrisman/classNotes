c357d6867116004ab14604b3b75800254016794f
Eric-Chrisman <118196908+Eric-Chrisman@users.noreply.github.com>
2024-01-25 10:46:27 -0600

    Update messingAround.r

--- r/messingAround.r
+++ r/messingAround.r
@@ -1,8 +1,14 @@
 # 1/25/24
+#https://posit.cloud online ide for r
+#https://rdrr.io/ r docs
+
 "Hello World"
 5 + 5
-library(foo)
+library(tidyverse)
 require(foo)
 
 rainfall.inches = 3 # dots can be used in variable names
 print(rainfull.inches)
+
+# my_table1 = read_csv("path")
+summary(table1)
